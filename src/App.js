89cee26e5b0a7282ebe8a9a7bdd5a571da275017
Sneha Lodha <snhthakare@gmail.com>
2020-07-21 17:59:37 +0530

    Updates sorting of table and dropdown

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { FormControl, MenuItem, Select  } from '@material-ui/core';
 import Infobox from './Infobox';
 import Map from './Map';
+import LineGraph from './LineGraph';
 import Table from './Table';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { sortData } from './util';
+import { FormControl, MenuItem, Select  } from '@material-ui/core';
 
 function App() {
 
   const [countries, setCountries] = useState([]);
-  const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState([]);
   const [tableData, setTableData] = useState([]);
 
@@ -37,14 +38,15 @@ function App() {
           value: country.countryInfo.iso2 //UK,USA,FR
           }));
 
-        const sortedData = sortData(data); //sort data by cases
-        setTableData(data);
+        const sortedData = sortData(data) //sort data by cases
+        setTableData(sortedData);
         setCountries(countries);
       })
     }
     getCountriesData();
   }, []);  
 
+  
   const onCountryChange = async (event) => {
     const countryCode = event.target.value;
     setCountry(countryCode);
@@ -67,34 +69,30 @@ function App() {
 
   return (
     <div className="app">
+      
       <div className="app__left">
           <div className="app__header">
             <h1>COVID-19 TRACKER</h1>
-            <FormControl className="app__dropdown">
-
-              <Select
-                variant="outlined"
-                value="abc"
-                onChange={onCountryChange}>
+            <FormControl className="app__dropdown"> 
+              <Select variant="outlined" value={country} onChange={onCountryChange}>
                 <MenuItem value="worldwide">Worldwide</MenuItem>
                 {
                   countries.map(country =>(
                     <MenuItem value={country.value}>{country.name}</MenuItem>
                   ))
                 }
-      
               </Select>
             </FormControl>
           </div>   
 
           <div className="app_stats">
-            <Infobox title="Coronavirus cases" total={countryInfo.todayCases} cases={countryInfo.todayCases} />
-            <Infobox title="Recovered" total={countryInfo.todayRecovered} cases={countryInfo.todayRecovered}/>
-            <Infobox title="Deaths"  total={countryInfo.todayDeaths} cases={countryInfo.todayDeaths}/>
+            <Infobox title="Coronavirus cases" total={countryInfo.cases} cases={countryInfo.todayCases} />
+            <Infobox title="Recovered" total={countryInfo.recovered} cases={countryInfo.todayRecovered}/>
+            <Infobox title="Deaths"  total={countryInfo.deaths} cases={countryInfo.todayDeaths}/>
 
           </div>
             
-           <Map />
+          <Map />
             
       </div>
       
@@ -102,10 +100,11 @@ function App() {
         <Card>
           <CardContent>
             <h2>Live cases by country</h2>
+            <h2>Worldwide new cases</h2>
             <Table countries={tableData} />
+            //<LineGraph />
           </CardContent>
           {/* Graph */}
-          {/* Tables */}
         </Card>
       </div>    
        
--- src/Table.js
+++ src/Table.js
@@ -6,12 +6,10 @@ function Table({countries}) {
   return (
     <div className="table">
       {countries.map(({country, cases}) => (
-
         <tr>
           <td>{country}</td>
           <td><strong>{cases}</strong></td>
         </tr>
-
       ))}
     </div>
   );
